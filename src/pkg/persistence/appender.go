098c5c2e2a4338cf4517e90c44a28dadebdc5a54
Mark Gangl <mgangl@pivotal.io>
2019-06-27 16:01:02 -0600

    use mutex for Appender internal points slice
    Co-authored-by: Johanna Ratliff <josmith@pivotal.io>

--- src/pkg/persistence/appender.go
+++ src/pkg/persistence/appender.go
@@ -6,6 +6,8 @@ import (
 	// _ "github.com/influxdata/influxdb/tsdb/engine"
 	// the go linter in some instances removes it
 
+	"sync"
+
 	_ "github.com/influxdata/influxdb/tsdb/engine"
 	"github.com/prometheus/prometheus/pkg/labels"
 
@@ -13,6 +15,7 @@ import (
 )
 
 type StoreAppender struct {
+	mu                    sync.Mutex
 	points                []*rpc.Point
 	adapter               *InfluxAdapter
 	metrics               AppenderMetrics
@@ -52,6 +55,8 @@ func WithLabelTruncationLength(length uint) WithAppenderOption {
 }
 
 func (a *StoreAppender) Add(l labels.Labels, time int64, value float64) (uint64, error) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	a.metrics.incIngress(1)
 
 	point := &rpc.Point{
@@ -70,6 +75,8 @@ func (s *StoreAppender) AddFast(l labels.Labels, ref uint64, t int64, v float64)
 }
 
 func (a *StoreAppender) Commit() error {
+	a.mu.Lock()
+	defer a.mu.Unlock()
 	err := a.adapter.WritePoints(a.points)
 	a.points = a.points[:0]
 	return err
