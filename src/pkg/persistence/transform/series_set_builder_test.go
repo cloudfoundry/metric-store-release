f2e043484f8bb0e441ebd953271658c503a57b0f
Hunter Williams <huwilliams@pivotal.io>
2019-10-14 14:39:02 -0600

    update series builder tests

--- src/pkg/persistence/transform/series_set_builder_test.go
+++ src/pkg/persistence/transform/series_set_builder_test.go
@@ -3,6 +3,7 @@ package transform_test
 import (
 	"github.com/cloudfoundry/metric-store-release/src/pkg/persistence/transform"
 	"github.com/influxdata/influxdb/query"
+	"github.com/prometheus/prometheus/pkg/labels"
 
 	"github.com/cloudfoundry/metric-store-release/src/pkg/testing"
 	. "github.com/onsi/ginkgo"
@@ -13,24 +14,44 @@ var _ = Describe("SeriesSetBuilder", func() {
 	Describe("creating a SeriesSet from Influx Points", func() {
 		It("adds influx point(s) to a single series", func() {
 			builder := transform.NewSeriesBuilder()
-			builder.AddInfluxPoint(
-				&query.FloatPoint{
-					Name:  "metric_name",
-					Time:  10000000,
-					Value: 99.0,
+			builder.AddPointsForSeries(
+				labels.FromMap(map[string]string{
+					"__name__":  "metric_name",
+					"source_id": "source_id",
+				}),
+				[]*query.FloatPoint{
+					&query.FloatPoint{
+						Name:  "metric_name",
+						Time:  10000000,
+						Value: 99.0,
+					},
+					&query.FloatPoint{
+						Name:  "metric_name",
+						Time:  15000000,
+						Value: 99.0,
+					},
+					&query.FloatPoint{
+						Name:  "metric_name",
+						Time:  20000000,
+						Value: 99.0,
+					},
 				},
-				[]string{},
 			)
-			Expect(builder.Len()).To(Equal(1))
+			Expect(builder.Len()).To(Equal(3))
 
 			seriesSet := builder.SeriesSet()
 			series := testing.ExplodeSeriesSet(seriesSet)
 
 			Expect(series).To(ConsistOf(
 				testing.Series{
-					Labels: map[string]string{"__name__": "metric_name"},
+					Labels: map[string]string{
+						"__name__":  "metric_name",
+						"source_id": "source_id",
+					},
 					Points: []testing.Point{
 						{Time: 10, Value: 99.0},
+						{Time: 15, Value: 99.0},
+						{Time: 20, Value: 99.0},
 					},
 				},
 			))
@@ -38,23 +59,27 @@ var _ = Describe("SeriesSetBuilder", func() {
 
 		It("adds influx points to multiple series", func() {
 			builder := transform.NewSeriesBuilder()
-			builder.AddInfluxPoint(
-				&query.FloatPoint{
+			builder.AddPointsForSeries(
+				labels.FromMap(map[string]string{
+					"__name__":  "metric_name",
+					"source_id": "source_id",
+				}),
+				[]*query.FloatPoint{&query.FloatPoint{
 					Name:  "metric_name",
 					Time:  10000000,
 					Value: 99.0,
-				},
-				[]string{},
+				}},
 			)
-			Expect(builder.Len()).To(Equal(1))
-
-			builder.AddInfluxPoint(
-				&query.FloatPoint{
-					Name:  "other_metric_name",
+			builder.AddPointsForSeries(
+				labels.FromMap(map[string]string{
+					"__name__":  "metric_name",
+					"source_id": "source_id_two",
+				}),
+				[]*query.FloatPoint{&query.FloatPoint{
+					Name:  "metric_name",
 					Time:  10000000,
 					Value: 99.0,
-				},
-				[]string{},
+				}},
 			)
 			Expect(builder.Len()).To(Equal(2))
 
@@ -63,13 +88,19 @@ var _ = Describe("SeriesSetBuilder", func() {
 
 			Expect(series).To(ConsistOf(
 				testing.Series{
-					Labels: map[string]string{"__name__": "metric_name"},
+					Labels: map[string]string{
+						"__name__":  "metric_name",
+						"source_id": "source_id",
+					},
 					Points: []testing.Point{
 						{Time: 10, Value: 99.0},
 					},
 				},
 				testing.Series{
-					Labels: map[string]string{"__name__": "other_metric_name"},
+					Labels: map[string]string{
+						"__name__":  "metric_name",
+						"source_id": "source_id_two",
+					},
 					Points: []testing.Point{
 						{Time: 10, Value: 99.0},
 					},
