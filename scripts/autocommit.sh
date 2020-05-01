700a8a716fdeab57b6a1809719e0f3ea9f2d6551
Joe Gee <jgee@pivotal.io>
2020-05-01 10:22:24 -0600

    Add unroll and diff options to autocommit.sh

--- scripts/autocommit.sh
+++ scripts/autocommit.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 
-if [[ $# -lt 1 ]]; then
+function squashHead() {
+  branch=$(git branch --show-current)
+  prevCommit=$(git log --oneline "${branch}" | grep -v '\[Autocommit\]' | head -1 | awk '{print $1}')
+
+  echo ${prevCommit}
+}
+
+function printUsage() {
   echo "Run tests and autocommit"
   echo "usage: $0 path/to/package/to/test"
   echo "       Watches that package for changes and automatically runs tests on changes"
@@ -9,6 +16,19 @@ if [[ $# -lt 1 ]]; then
   echo "Squash autocommits"
   echo "usage: $0 squash \"commit message\""
   echo "       Squashes last concurrent [Autocommit] tagged commits into a single commit"
+  echo ""
+  echo "Diff squash"
+  echo "usage: $0 diff"
+  echo "       show the diff of all that you are about to squash but don't squash"
+  echo ""
+  echo "Unroll commits"
+  echo "usage: $0 unroll"
+  echo "       unroll your autocommits but do not automatically commit the squash"
+
+}
+
+if [[ $# -lt 1 || $1 == "-h" ]]; then
+  printUsage
   exit 1
 fi
 
@@ -17,12 +37,15 @@ projectdir=$(git rev-parse --show-toplevel)
 testdir="${projectdir}/$1"
 
 if [[ $1 == "squash" ]]; then
-  branch=$(git branch --show-current)
-  prevCommit=$(git log --oneline "${branch}" | grep -v '\[Autocommit\]' | head -1 | awk '{print $1}')
-
-  git reset "$prevCommit"
+  git reset "$(squashHead)"
   git add -A
   git commit -m "$2"
+elif [[ $1 == "unroll" ]]; then
+  git reset "$(squashHead)"
+  git add -A
+  git commit -m "$2"
+elif [[ $1 == "diff" ]]; then
+  git diff "$(squashHead)"
 else
   if [ ! -d "${testdir}" ]; then
     echo "path ${testdir} does not exist"
