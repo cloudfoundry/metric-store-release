41beec6227d7bfe8f229f6da9f0b94f8dd93b451
Peter Stone <pstone@pivotal.io>
2020-06-29 16:32:25 -0600

    backfill tests for cluster-discovery app

--- src/cmd/cluster-discovery/app/cluster_discovery_test.go
+++ src/cmd/cluster-discovery/app/cluster_discovery_test.go
@@ -1,81 +1,94 @@
 package app_test
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/cloudfoundry/metric-store-release/src/cmd/cluster-discovery/app"
+	"github.com/cloudfoundry/metric-store-release/src/internal/metrics"
+	"github.com/cloudfoundry/metric-store-release/src/internal/testing"
+	"github.com/cloudfoundry/metric-store-release/src/internal/tls"
+	"github.com/cloudfoundry/metric-store-release/src/pkg/logger"
 )
 
 var _ = Describe("Cluster Discovery App", func() {
 	var (
-	//clusterDiscovery *app.ClusterDiscoveryApp
+		clusterDiscovery *app.ClusterDiscoveryApp
 	)
 
 	BeforeEach(func() {
-		//metricStore = app.NewMetricStoreApp(&app.Config{
-		//	TLS: tls.TLS{
-		//		CAPath:   testing.Cert("metric-store-ca.crt"),
-		//		CertPath: testing.Cert("metric-store.crt"),
-		//		KeyPath:  testing.Cert("metric-store.key"),
-		//	},
-		//	MetricStoreServerTLS: app.MetricStoreServerTLS{
-		//		CAPath:   testing.Cert("metric-store-ca.crt"),
-		//		CertPath: testing.Cert("metric-store.crt"),
-		//		KeyPath:  testing.Cert("metric-store.key"),
-		//	},
-		//	MetricStoreInternodeTLS: app.MetricStoreInternodeTLS{
-		//		CAPath:   testing.Cert("metric-store-ca.crt"),
+		clusterDiscovery = app.NewClusterDiscoveryApp(&app.Config{
+			MetricsAddr:     "",
+			ProfilingAddr:   "",
+			StoragePath:     "/tmp/metric-store",
+			LogLevel:        "",
+			MetricsTLS:      app.ClusterDiscoveryMetricsTLS{},
+			MetricStoreAPI:  app.MetricStoreAPI{},
+			PKS:             app.PKSConfig{},
+			UAA:             app.UAAConfig{},
+			RefreshInterval: 0,
+		}, logger.NewTestLogger(GinkgoWriter))
+		go clusterDiscovery.Run()
 
-		//		CertPath: testing.Cert("metric-store.crt"),
-		//		KeyPath:  testing.Cert("metric-store.key"),
-		//	},
-		//	MetricStoreMetricsTLS: app.MetricStoreMetricsTLS{
-		//		CAPath:   testing.Cert("metric-store-ca.crt"),
-		//		CertPath: testing.Cert("metric-store.crt"),
-		//		KeyPath:  testing.Cert("metric-store.key"),
-		//	},
-		//	StoragePath: "/tmp/metric-store",
-		//}, logger.NewTestLogger(GinkgoWriter))
-		//go metricStore.Run()
-		//
-		//Eventually(metricStore.HealthAddr).ShouldNot(BeEmpty())
+		Eventually(clusterDiscovery.MetricsAddr).ShouldNot(BeEmpty())
 	})
 
 	AfterEach(func() {
-		//defer metricStore.Stop()
+		defer clusterDiscovery.Stop()
 	})
 
 	It("serves metrics on a metrics endpoint", func() {
-		//var body string
-		//
-		//tlsConfig, err := tls.NewMutualTLSClientConfig(
-		//	testing.Cert("metric-store-ca.crt"),
-		//	testing.Cert("metric-store.crt"),
-		//	testing.Cert("metric-store.key"),
-		//	"metric-store",
-		//)
-		//Expect(err).ToNot(HaveOccurred())
-		//
-		//httpClient := &http.Client{
-		//	Transport: &http.Transport{TLSClientConfig: tlsConfig},
-		//}
-		//
-		//fn := func() string {
-		//	resp, err := httpClient.get("https://" + metricStore.HealthAddr() + "/metrics")
-		//	if err != nil {
-		//		return ""
-		//	}
-		//	defer resp.Body.Close()
-		//
-		//	bytes, err := ioutil.ReadAll(resp.Body)
-		//	if err != nil {
-		//		return ""
-		//	}
-		//
-		//	body = string(bytes)
-		//
-		//	return body
-		//}
-		//Eventually(fn).ShouldNot(BeEmpty())
-		//Expect(body).To(ContainSubstring(debug.MetricStoreWrittenPointsTotal))
-		//Expect(body).To(ContainSubstring("go_threads"))
+		var body string
+
+		tlsConfig, err := tls.NewMutualTLSClientConfig(
+			testing.Cert("metric-store-ca.crt"),
+			testing.Cert("metric-store.crt"),
+			testing.Cert("metric-store.key"),
+			"metric-store",
+		)
+		Expect(err).ToNot(HaveOccurred())
+
+		httpClient := &http.Client{
+			Transport: &http.Transport{TLSClientConfig: tlsConfig},
+		}
+
+		fn := func() string {
+			resp, err := httpClient.Get("https:" + clusterDiscovery.MetricsAddr() + "/metrics")
+			if err != nil {
+				return ""
+			}
+			defer func() { _ = resp.Body.Close() }()
+
+			bytes, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				return ""
+			}
+
+			body = string(bytes)
+
+			return body
+		}
+		Eventually(fn).ShouldNot(BeEmpty())
+		Expect(body).To(ContainSubstring(metrics.MetricStoreWrittenPointsTotal))
+		Expect(body).To(ContainSubstring("go_threads"))
+	})
+
+	It("listens with pprof", func() {
+		callPprof := func() int {
+
+			resp, err := http.Get("http://" + clusterDiscovery.ProfilingAddr() + "/debug/pprof")
+			if err != nil {
+				fmt.Printf("calling pprof: %s\n", err)
+				return -1
+			}
+			defer func() { _ = resp.Body.Close() }()
+
+			return resp.StatusCode
+		}
+		Eventually(callPprof).Should(Equal(200))
 	})
 })
