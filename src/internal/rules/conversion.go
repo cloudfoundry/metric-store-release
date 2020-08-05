6dfe82d1e757af5d4a9001a1a92cef835025ea94
Mark Gangl <mgangl@pivotal.io>
2020-08-05 18:09:38 +0000

    WIP

--- src/internal/rules/conversion.go
+++ src/internal/rules/conversion.go
@@ -1,14 +1,13 @@
 package rules
 
 import (
-	"context"
-	html_template "html/template"
-	"net/url"
+	"encoding/json"
+	"errors"
 	"time"
 
 	prom_versioned_api_client "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/prometheus/pkg/labels"
-	"github.com/prometheus/prometheus/promql"
+	"github.com/prometheus/prometheus/promql/parser"
 	"github.com/prometheus/prometheus/rules"
 )
 
@@ -30,17 +29,42 @@ func ConvertAPIRuleGroupToGroup(ruleGroups []prom_versioned_api_client.RuleGroup
 	return groups
 }
 
-// NEEDS to fulfill api
-// Name:           grp.Name(),
-// File:           grp.File(),
-// Interval:       grp.Interval().Seconds(),
-// Rules:          []rule{},
-// EvaluationTime: grp.GetEvaluationDuration().Seconds(),
-// LastEvaluation: grp.GetEvaluationTimestamp(),
+func ConvertAPIRulesToRule(apiRules prom_versioned_api_client.Rules) []rules.Rule {
+	var rs []rules.Rule
 
-// grp.Rules()
-// -> r.Type
+	for _, apiRule := range apiRules {
+		switch r := apiRule.(type) {
+		case prom_versioned_api_client.AlertingRule:
+			rs = append(rs, rules.NewAlertingRule(r.Name, nil, 0,
+				nil, nil, nil,
+				true, nil))
+			//	vec parser.Expr, hold time.Duration,
+			//	labels, annotations, externalLabels labels.Labels,
+			//	restored bool, logger log.Logger,
+
+		case prom_versioned_api_client.RecordingRule:
+			expr, _ := parser.ParseExpr(r.Query)
+
+			l2, _ := json.Marshal(r.Labels)
+			ls := labels.Labels{}
+			ls.UnmarshalJSON(l2)
+
+			rule := rules.NewRecordingRule(r.Name, expr, ls)
+			rule.SetHealth(rules.RuleHealth(r.Health))
+			rule.SetLastError(errors.New(r.LastError))
+			rs = append(rs, rule)
+		}
+	}
+
+	return rs
+}
+
+// switch rule := r.(type) {
 // case *rules.AlertingRule:
+// 	if !returnAlerts {
+// 		break
+// 	}
+// 	enrichedRule = alertingRule{
 // 		State:          rule.State().String(),
 // 		Name:           rule.Name(),
 // 		Query:          rule.Query().String(),
@@ -53,7 +77,12 @@ func ConvertAPIRuleGroupToGroup(ruleGroups []prom_versioned_api_client.RuleGroup
 // 		EvaluationTime: rule.GetEvaluationDuration().Seconds(),
 // 		LastEvaluation: rule.GetEvaluationTimestamp(),
 // 		Type:           "alerting",
+// 	}
 // case *rules.RecordingRule:
+// 	if !returnRecording {
+// 		break
+// 	}
+// 	enrichedRule = recordingRule{
 // 		Name:           rule.Name(),
 // 		Query:          rule.Query().String(),
 // 		Labels:         rule.Labels(),
@@ -62,98 +91,4 @@ func ConvertAPIRuleGroupToGroup(ruleGroups []prom_versioned_api_client.RuleGroup
 // 		EvaluationTime: rule.GetEvaluationDuration().Seconds(),
 // 		LastEvaluation: rule.GetEvaluationTimestamp(),
 // 		Type:           "recording",
-
-type conversionRule struct {
-	name string
-	// Query       string
-	// Duration    float64
-	// Labels      model.LabelSet
-	// Annotations model.LabelSet
-	// Alerts      []*prom_versioned_api_client.Alert
-	// Health      prom_versioned_api_client.RuleHealth
-	// LastError   string
-}
-
-// func NewConversionRule(name string) conversionRule {
-// 	return conversionRule{
-// }
-
-func (c conversionRule) Name() string {
-	return c.name
-}
-
-// Labels of the rule.
-func (c conversionRule) Labels() labels.Labels {
-	panic("not implemented") // TODO: Implement
-}
-
-// eval evaluates the rule, including any associated recording or alerting actions.
-func (c conversionRule) Eval(_ context.Context, _ time.Time, _ rules.QueryFunc, _ *url.URL) (promql.Vector, error) {
-	panic("not implemented") // TODO: Implement
-}
-
-// String returns a human-readable string representation of the rule.
-func (c conversionRule) String() string {
-	panic("not implemented") // TODO: Implement
-}
-
-// SetLastErr sets the current error experienced by the rule.
-func (c conversionRule) SetLastError(_ error) {
-	panic("not implemented") // TODO: Implement
-}
-
-// LastErr returns the last error experienced by the rule.
-func (c conversionRule) LastError() error {
-	panic("not implemented") // TODO: Implement
-}
-
-// SetHealth sets the current health of the rule.
-func (c conversionRule) SetHealth(_ rules.RuleHealth) {
-	panic("not implemented") // TODO: Implement
-}
-
-// Health returns the current health of the rule.
-func (c conversionRule) Health() rules.RuleHealth {
-	panic("not implemented") // TODO: Implement
-}
-
-func (c conversionRule) SetEvaluationDuration(_ time.Duration) {
-	panic("not implemented") // TODO: Implement
-}
-
-// GetEvaluationDuration returns last evaluation duration.
-// NOTE: Used dynamically by rules.html template.
-func (c conversionRule) GetEvaluationDuration() time.Duration {
-	panic("not implemented") // TODO: Implement
-}
-
-func (c conversionRule) SetEvaluationTimestamp(_ time.Time) {
-	panic("not implemented") // TODO: Implement
-}
-
-// GetEvaluationTimestamp returns last evaluation timestamp.
-// NOTE: Used dynamically by rules.html template.
-func (c conversionRule) GetEvaluationTimestamp() time.Time {
-	panic("not implemented") // TODO: Implement
-}
-
-// HTMLSnippet returns a human-readable string representation of the rule,
-// decorated with HTML elements for use the web frontend.
-func (c conversionRule) HTMLSnippet(pathPrefix string) html_template.HTML {
-	panic("not implemented") // TODO: Implement
-}
-
-func ConvertAPIRulesToRule(apiRules prom_versioned_api_client.Rules) []rules.Rule {
-	var rules []rules.Rule
-
-	for _, apiRule := range apiRules {
-		switch r := apiRule.(type) {
-		case prom_versioned_api_client.RecordingRule:
-			rules = append(rules, conversionRule{
-				name: r.Name,
-			})
-		}
-	}
-
-	return rules
-}
+// 	}
--- src/internal/rules/conversion_test.go
+++ src/internal/rules/conversion_test.go
@@ -3,13 +3,16 @@ package rules_test
 import (
 	. "github.com/cloudfoundry/metric-store-release/src/internal/rules"
 	prom_versioned_api_client "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
+	"github.com/prometheus/prometheus/pkg/labels"
+	prom_rules "github.com/prometheus/prometheus/rules"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Conversion", func() {
-	FDescribe("#ConvertAPIRuleGroupToGroup", func() {
+var _ = FDescribe("Conversion", func() {
+	Describe("#ConvertAPIRuleGroupToGroup", func() {
 		It("converts API RuleGroups to Groups", func() {
 			apiRecordingRule := prom_versioned_api_client.RecordingRule{
 				Name: "recording rule",
@@ -41,9 +44,13 @@ var _ = Describe("Conversion", func() {
 	})
 
 	Describe("#ConvertAPIRulesToRule", func() {
-		It("converts API Rules to Rules", func() {
+		It("converts API Recording Rules to Rules", func() {
 			apiRecordingRule := prom_versioned_api_client.RecordingRule{
-				Name: "recording rule",
+				Name:      "recording rule",
+				Query:     "1+2",
+				Labels:    map[model.LabelName]model.LabelValue{"labelKey": "labelValue"},
+				Health:    prom_versioned_api_client.RuleHealthGood,
+				LastError: "oops",
 			}
 
 			rules := ConvertAPIRulesToRule(
@@ -53,7 +60,27 @@ var _ = Describe("Conversion", func() {
 			Expect(len(rules)).To(Equal(1))
 
 			rule := rules[0]
+			Expect(rule).To(BeAssignableToTypeOf(&prom_rules.RecordingRule{}))
 			Expect(rule.Name()).To(Equal("recording rule"))
+			Expect(rule.(*prom_rules.RecordingRule).Query().String()).To(Equal("1 + 2"))
+			Expect(rule.Labels()).To(Equal(labels.FromMap(map[string]string{"labelKey": "labelValue"})))
+			Expect(rule.Health()).To(Equal(prom_rules.HealthGood))
+			Expect(rule.LastError()).To(MatchError("oops"))
+		})
+		It("converts API Alerting Rules to Rules", func() {
+			apiAlertingRule := prom_versioned_api_client.AlertingRule{
+				Name: "alerting rule",
+			}
+
+			rules := ConvertAPIRulesToRule(
+				prom_versioned_api_client.Rules{apiAlertingRule},
+			)
+
+			Expect(len(rules)).To(Equal(1))
+
+			rule := rules[0]
+			Expect(rule).To(BeAssignableToTypeOf(&prom_rules.AlertingRule{}))
+			Expect(rule.Name()).To(Equal("alerting rule"))
 		})
 	})
 })
