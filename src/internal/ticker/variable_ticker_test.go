d0f8059247fb980aa3a7ea9b5256db44e94cc220
Sargis Boyajyan <sboyajyan@sboyajyanCXV7T.vmware.com>
2023-02-16 16:38:01 +0400

    src/internal/ticker/variable_ticker_test.go

--- src/internal/ticker/variable_ticker_test.go
+++ src/internal/ticker/variable_ticker_test.go
@@ -43,7 +43,7 @@ var _ = Describe("A ticker for exponential backoffs", func() {
 		Consistently(ticker.C).ShouldNot(Receive())
 	})
 
-	FIt("stops", func() {
+	It("stops", func() {
 		delay := NewExponentialDelay(&Config{MaxDelay: 10 * time.Microsecond})
 		ticker := New(delay)
 
