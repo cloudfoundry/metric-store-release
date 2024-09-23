a956816a7040e4c90b44b69728db0a0dc90035af
Srinivas Sunka <srinivas.sunka@broadcom.com>
2024-09-23 15:57:23 +0530

    Starting GRPC server and removing tls tcp listener

--- src/internal/nozzle/otel_server.go
+++ src/internal/nozzle/otel_server.go
@@ -3,16 +3,18 @@ package nozzle
 import (
 	"context"
 	"crypto/tls"
+	"fmt"
+	"net"
+
 	metricspb "go.opentelemetry.io/proto/otlp/collector/metrics/v1"
 	tracepb "go.opentelemetry.io/proto/otlp/collector/trace/v1"
 	"go.uber.org/zap"
 
 	_ "google.golang.org/grpc/encoding/gzip"
-	"net"
-	"time"
 
 	"github.com/cloudfoundry/metric-store-release/src/pkg/logger"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 type OtelServer struct {
@@ -40,8 +42,7 @@ func (s *OtelServer) Close() error {
 	s.ms.Stop()
 	s.ts.Stop()
 
-	// Optionally, reset s.grpcServer to nil to indicate it's closed
-	// This is useful if you're checking the state of the server elsewhere
+	// Reset s.grpcServer to nil to indicate it's closed
 	s.grpcServer = nil
 
 	s.cancel()
@@ -52,18 +53,21 @@ func (s *OtelServer) Close() error {
 
 func (s *OtelServer) Start(addr string, otelTlsConfig *tls.Config) {
 	s.log.Info("OtelServer starting grpc server at ", zap.String("address", addr))
-	go s.startGRPCServer(addr, otelTlsConfig)
 
-	s.log.Info("Registering Metrics and Trace Server")
-	metricspb.RegisterMetricsServiceServer(s.grpcServer, s.ms)
-	tracepb.RegisterTraceServiceServer(s.grpcServer, s.ts)
+	// Initialize gRPC server with TLS credentials
+	grpcServer := grpc.NewServer(grpc.Creds(credentials.NewTLS(otelTlsConfig)))
 
-	s.log.Info("Starting Metrics Server")
+	// Register Metric and Trace services with the gRPC server
+	metricspb.RegisterMetricsServiceServer(grpcServer, s.ms)
+	tracepb.RegisterTraceServiceServer(grpcServer, s.ts)
+
+	// Start the gRPC server in a separate goroutine
+	go s.startGRPCServer(addr, grpcServer)
+
+	s.log.Info("Starting Metrics and Trace Servers")
 	s.ms.StartListening()
-	s.log.Info("Starting Trace Server")
 	s.ts.StartListening()
 	s.log.Info("OtelServer started")
-
 }
 
 func NewOtelServer(
@@ -71,18 +75,13 @@ func NewOtelServer(
 	ms *MetricService,
 	ts *TraceService,
 ) *OtelServer {
-
 	ctx, cancel := context.WithCancel(context.Background())
-	// Initialize the gRPC server and register the metric service
-	grpcServer := grpc.NewServer()
 
 	// Return a new OtelServer instance containing the gRPC server and other relevant info
 	return &OtelServer{
-		grpcServer: grpcServer,
-		log:        log,
-		ms:         ms,
-		ts:         ts,
-
+		log:    log,
+		ms:     ms,
+		ts:     ts,
 		ctx:    ctx,
 		cancel: cancel,
 		done:   make(chan struct{}, 1),
@@ -90,33 +89,27 @@ func NewOtelServer(
 }
 
 // StartGRPCServer starts the gRPC server and listens for incoming connections.
-func (s *OtelServer) startGRPCServer(addr string, otelTlsConfig *tls.Config) {
+func (s *OtelServer) startGRPCServer(addr string, grpcServer *grpc.Server) {
 	defer func() {
 		close(s.done)
 	}()
 
-	address, err := net.ResolveTCPAddr("tcp", s.addr)
+	address := fmt.Sprintf("0.0.0.0:%s", addr)
+	resolvedAddr, err := net.ResolveTCPAddr("tcp", address)
 	if err != nil {
 		s.log.Panic("Failed to resolve address", zap.Error(err))
 	}
 
-	listener, err := tls.Listen("tcp", address.String(), otelTlsConfig)
+	listener, err := net.ListenTCP("tcp", resolvedAddr)
 	if err != nil {
 		s.log.Panic("Failed to start listener", zap.Error(err))
 	}
 	defer listener.Close()
 
-	for {
-		// Accept incoming TCP connection
-		conn, err := listener.Accept()
-		if err != nil {
-			s.log.Error("Error while accepting connection", err)
-			err := conn.Close()
-			if err != nil {
-				return
-			}
-			continue
-		}
-		time.Sleep(time.Second * 5)
+	s.log.Info("Listening on", zap.String("address", resolvedAddr.String()))
+
+	// Serve the gRPC server on the TCP listener
+	if err := grpcServer.Serve(listener); err != nil {
+		s.log.Panic("gRPC server failed", zap.Error(err))
 	}
 }
